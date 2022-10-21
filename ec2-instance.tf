ab56fbfb62ff8645233cedb9d45a8f5d21d8ef87
burnling <khaclinh.tk42@gmail.com>
2022-10-21 22:39:56 +0700

    update

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -10,8 +10,8 @@ module "ec2_instance" {
   instance_type          = var.instance_type
   key_name               = var.key_name
   monitoring             = false
-  vpc_security_group_ids = var.vpc_security_group_ids
-  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.vpc_security_group_ids]
+  subnet_id              = [var.subnet_id]
 
   tags = {
     Terraform   = "true"
