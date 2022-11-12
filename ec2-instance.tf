bc2a9de879e137384eed1f04b8276ebf9cc5328d
burnling <khaclinh.tk42@gmail.com>
2022-11-12 21:17:04 +0700

    update

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,6 +1,6 @@
 module "panthera_instance" {
-  source      = "cloudposse/terraform-aws-ec2-instance"
-  version     = "0.44.0"
+  source  = "app.terraform.io/provisioning-infra-aws/ec2-instance/aws"
+  version = "1.0.0"
 
   #for_each = toset(["one", "two", "three"])
   #name = "instance-${each.key}"
