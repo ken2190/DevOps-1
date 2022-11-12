bd01382df6cadffb17e966b5f301fea9b6fc4d0e
burnling <khaclinh.tk42@gmail.com>
2022-11-12 21:08:04 +0700

    update

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,5 +1,5 @@
 module "panthera_instance" {
-  source = "linhnk1/terraform-aws-ec2-instance"
+  source      = "provisioning-infra-aws/Registry/Modules/private/ec2-instance/aws"
   version     = "1.0.0"
 
   #for_each = toset(["one", "two", "three"])
